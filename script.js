c1a4211112f26a3b8f3e98bb052ea5f462eb302a
Roman Geathers <romangeathersdev@gmail.com>
2022-05-04 18:18:31 -0400

    Get player selection and check who won
    Finish function that gets the player's selection. Then Finish
    the function that check whether the player or computer won.

--- script.js
+++ script.js
@@ -21,3 +21,40 @@ function computerPlay() {
         return 'Scissors';
     }
 }
+
+//Sees if player or computer won
+function oneRound() {
+    let playerSelection = getPlayerSelection();
+    let computerSelection = computerPlay();
+
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        console.log('The computer threw ' + computerSelection + '. It\'s a tie!');
+    } 
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        console.log('The computer threw ' + computerSelection + '. Paper beats rock!');
+    }
+    else if (computerSelection === 'rock' && playerSelection === 'paper') {
+        console.log('The computer threw ' + computerSelection + '. Paper beats rock!');
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log('The computer threw ' + computerSelection + '. Scissors beats paper!');
+    }
+    else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        console.log('The computer threw ' + computerSelection + '. Scissors beats paper!');
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        console.log('The computer threw ' + computerSelection + '. Rock beats scissors!');
+    }
+    else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+        console.log('The computer threw ' + computerSelection + '. Rock beats scissors!');
+    }
+}
+
+//get player's selection
+function getPlayerSelection() {
+    let selection = prompt('Please enter \'rock\', \'paper\', or \'scissors\'.');
+    return selection;
+}
