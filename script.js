68c8b1ac7e374555176c26364408c938fc015b46
Roman Geathers <romangeathersdev@gmail.com>
2022-05-04 15:39:13 -0400

    Set random number choice between 1 and 99
    Fix random number choice to set between 1 and 99 so that
    computer is equally likely to choose any of the three rock paper
    scissors choices.

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+//randomly return rock, paper, or scissors
 function computerPlay() {
     let numChoice = Math.random();
     numChoice = numChoice.toFixed(2);
     numChoice *= 100;
-    if (numChoice >= 0 && numChoice < 33) {
+    //set choice between 1 and 99
+    if (numChoice === 100) {
+        numChoice -= 1;
+    }
+    if (numChoice === 0) {
+        numChoice += 1;
+    }
+    // return choice
+    if (numChoice >= 1 && numChoice <= 33) {
         return 'Rock';
     } 
-    else if (numChoice >= 33 && numChoice <= 66) {
+    else if (numChoice >= 34 && numChoice <= 66) {
         return 'Paper';
     }
     else {
